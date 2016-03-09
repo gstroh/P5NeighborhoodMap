8c05a0f34189bafaf3446c17b29789b987bf7055
Gregg Strohkorb <greggstrohkorb@Greggs-MacBook-Pro-44.local>
2016-03-09 09:46:42 -0600

    Fixed interaction between search query and legend check boxes.

--- src/js/app.js
+++ src/js/app.js
@@ -5,7 +5,7 @@ var myMapApp = myMapApp || { };
 
 myMapApp.viewModel = function() {
     // initialize some variables
-    console.log("viewModel");
+    //console.log("viewModel");
     myMapApp.Jerusalem = new google.maps.LatLng(31.776347, 35.231446);
     myMapApp.infowindow = null;
     myMapApp.markers = [];
@@ -16,16 +16,20 @@ myMapApp.viewModel = function() {
     myMapApp.FilteredList = ko.observableArray();
     myMapApp.flickrPhotos = [];
     myMapApp.noGooglePages = 0;
-    // new data structure in array with these fields: placeIcon, googlePlaceType, displayPlaceType.
-    myMapApp.googleTypes = ['church', 'mosque', 'museum', 'place_of_worship', 'synagogue', 'park'];
+
+    // ------- Model -------
+
+    // The model is defined here with an array of Google place types used in getPlacesFromGoogleMaps
+    myMapApp.googleTypes = [];
+    // The folowing knockout array is also used to define place types, checked boxes and icons used in the application
     myMapApp.placeTypes = ko.observableArray();
     // add plave types to ko.observable array
-    myMapApp.placeTypes.push({displayPlaceType: "Church", checkedPlace: ko.observable(true), placeIcon: "http://maps.google.com/mapfiles/ms/icons/blue-dot.png"});
-    myMapApp.placeTypes.push({displayPlaceType: "Mosque", checkedPlace: ko.observable(true), placeIcon: "http://maps.google.com/mapfiles/ms/icons/green-dot.png"});
-    myMapApp.placeTypes.push({displayPlaceType: "Museum", checkedPlace: ko.observable(true), placeIcon: "http://maps.google.com/mapfiles/ms/icons/orange-dot.png"});
-    myMapApp.placeTypes.push({displayPlaceType: "Place of Worship", checkedPlace: ko.observable(true), placeIcon: "http://maps.google.com/mapfiles/ms/icons/pink-dot.png"});
-    myMapApp.placeTypes.push({displayPlaceType: "Synagogue", checkedPlace: ko.observable(true), placeIcon: "http://maps.google.com/mapfiles/ms/icons/yellow-dot.png"});
-    myMapApp.placeTypes.push({displayPlaceType: "Park", checkedPlace: ko.observable(true), placeIcon: "http://maps.google.com/mapfiles/ms/icons/purple-dot.png"});
+    myMapApp.placeTypes.push({displayPlaceType: "Church", googleType: "church", checkedPlace: ko.observable(true), placeIcon: "http://maps.google.com/mapfiles/ms/icons/blue-dot.png"});
+    myMapApp.placeTypes.push({displayPlaceType: "Mosque", googleType: "mosque", checkedPlace: ko.observable(true), placeIcon: "http://maps.google.com/mapfiles/ms/icons/green-dot.png"});
+    myMapApp.placeTypes.push({displayPlaceType: "Museum", googleType: "museum", checkedPlace: ko.observable(true), placeIcon: "http://maps.google.com/mapfiles/ms/icons/orange-dot.png"});
+    myMapApp.placeTypes.push({displayPlaceType: "Place of Worship", googleType: "place_of_worship", checkedPlace: ko.observable(true), placeIcon: "http://maps.google.com/mapfiles/ms/icons/pink-dot.png"});
+    myMapApp.placeTypes.push({displayPlaceType: "Synagogue", googleType: "synagogue", checkedPlace: ko.observable(true), placeIcon: "http://maps.google.com/mapfiles/ms/icons/yellow-dot.png"});
+    myMapApp.placeTypes.push({displayPlaceType: "Park", googleType: "park", checkedPlace: ko.observable(true), placeIcon: "http://maps.google.com/mapfiles/ms/icons/purple-dot.png"});
 
 // The init function includes the model, view model and view.  The model is generated using Google
 // Maps API.  Locations are gathered based on a 2 mile radius from a center point in Jerusalem and the
@@ -33,12 +37,15 @@ myMapApp.viewModel = function() {
 // of the view model with observables.  Therefore, in the application it is impossible to clearly split
 // out the model, view model and view.
 
+
     // initialize the View Model
     myMapApp.init = function() {
       myMapApp.createMap();
       myMapApp.subscribePlaceTypes();
+      myMapApp.getGooglePlaceTypes();
       myMapApp.getPlacesFromGoogleMaps();
     };
+
 
     // create the Google map
     myMapApp.createMap = function(){
@@ -59,6 +66,7 @@ myMapApp.viewModel = function() {
         });
     };
 
+
     // Create a knockout subscribe to changes to the array element myMapApp.placeTypes.checkedPlace.
     // These buttons appear in the legend at the bottom of the screen.
     myMapApp.subscribePlaceTypes = function () {
@@ -69,14 +77,103 @@ myMapApp.viewModel = function() {
       }
     }
 
+
     // a change has occurred to the checkedPlace field in the legend
     myMapApp.checkedPlaceChange = function(NewValue){
-      alert('An items name property change to '+NewValue);
-      // create a computed list for each place type
-      // loop over the place types
-      // save each computed list in an array (push)
-      // or, just push to FilterList as go along
+      console.log("checkedPlaceChange", NewValue);
+      // set search query to null string
+      //var sq = ko.observable("query");
+      // if (myMapApp.query() != "") {
+      //   console.log("query = ", myMapApp.query());
+      //   myMapApp.query("");
+      // }
+      // myMapApp.deleteMarkers();
+      // myMapApp.FilteredList.removeAll();
+      // myMapApp.CompleteList.removeAll();
+      // myMapApp.getGooglePlaceTypes();
+      // myMapApp.getPlacesFromGoogleMaps();
+
+      // myMapApp.query("");
+      // //alert('An items name property change to '+NewValue);
+      // create a copy of FilteredList with types checked in legend
+      var NewFilteredList = ko.observableArray();
+      for (var i = 0; i < myMapApp.CompleteList().length; i++) {
+          // console.log("FilteredList = ", myMapApp.FilteredList());
+          // console.log("googleTypes = ", myMapApp.googleTypes);
+          // console.log("place types = ", myMapApp.placeTypes());
+          for (var j = 0; j < myMapApp.placeTypes().length; j++) {
+            // console.log("myMapApp.placeTypes()[j].checkedPlace = ", myMapApp.placeTypes()[j].checkedPlace());
+            // console.log("myMapApp.FilteredList()[i].types[0] = ", myMapApp.FilteredList()[i].types[0]);
+            // console.log("myMapApp.googleTypes[j] = ", myMapApp.googleTypes[j]);
+            if (myMapApp.placeTypes()[j].checkedPlace()  == true &&
+                myMapApp.CompleteList()[i].types[0] == myMapApp.googleTypes[j]) {
+              // console.log("** PUSH **");
+              NewFilteredList.push( myMapApp.CompleteList()[i] );
+            }
+          }
+      }
+      console.log("display New FilteredList");
+      myMapApp.displayFilteredList(NewFilteredList);
+    };
+
+
+    // set the checked place field in the legend to true
+    myMapApp.setCheckedPlaceTrue = function () {
+      console.log("set CheckedPlaceTrue");
+      for (var i = 0; i < myMapApp.placeTypes().length; i++) {
+        if (myMapApp.placeTypes()[i].checkedPlace() == false) {
+          myMapApp.placeTypes()[i].checkedPlace(true);
+        }
+      }
     }
+
+
+    // set the checked place field in the legend to logical input (true/false)
+    myMapApp.setCheckedPlaceDisabled = function (logical) {
+      console.log("set CheckedPlaceDisabled");
+
+      var legend = document.getElementsByClassName("legendCheckBox");
+      console.log("legend = ", legend);
+      for (var i = 0; i < legend.length; i++) {
+        legend[i].disabled = logical;
+      }
+    }
+
+
+    // display the Filtered List
+    myMapApp.displayFilteredList = function (NewList) {
+      // remove existing Filtered List
+      myMapApp.FilteredList.removeAll();
+      // replace Filtered List with new list
+      for (var i = 0; i < NewList().length; i++) {
+        myMapApp.FilteredList.push( NewList()[i] );
+      }
+      // Set the markers with the filtered locations
+      myMapApp.deleteMarkers();
+      myMapApp.bounds = new google.maps.LatLngBounds();
+      for (var i = 0; i < myMapApp.FilteredList().length; i++) {
+        var place = myMapApp.FilteredList()[i].place;
+        place.marker = myMapApp.setMapMarker(myMapApp.FilteredList()[i].place);
+        myMapApp.bounds.extend(new google.maps.LatLng(
+            place.geometry.location.lat(),
+            place.geometry.location.lng()));
+      }
+      // center the map
+      myMapApp.centerMap();
+      console.log("finished displayFiltered List");
+    }
+
+
+    // set the myMapApp.googleTypes array based on the options chosen by the user in the legend
+    myMapApp.getGooglePlaceTypes = function () {
+      myMapApp.googleTypes = [];
+      for (var i = 0; i < myMapApp.placeTypes().length; i++) {
+        if (myMapApp.placeTypes()[i].checkedPlace()  == true ) {
+          myMapApp.googleTypes.push(myMapApp.placeTypes()[i].googleType);
+        }
+      }
+      //console.log("myMapApp.googleTypes = ", myMapApp.googleTypes);
+    };
 
     // Get map locations from Google maps by doing a nearbySearch of Jerusalem for a radius of 2 miles.
     // This call is asynchronous, using callback processGoogleResults.
@@ -112,6 +209,7 @@ myMapApp.viewModel = function() {
         for (var i = results.length - 1; i >= 0; i--) {
           var place = results[i];
           var placeName = place.name;
+          //console.log("placeName = ", placeName);
           var placeAddress = myMapApp.setAddress(place);
           // Test to see if Hebrew place name and remove it.
           if (myMapApp.isHebrew(placeName)) {
@@ -172,6 +270,7 @@ myMapApp.viewModel = function() {
       //console.log("myMapApp.markers = ", myMapApp.markers);
     };
 
+
     // is the text string Hebrew?
     myMapApp.isHebrew = function (text) {
       var rvalue = false;
@@ -185,9 +284,12 @@ myMapApp.viewModel = function() {
     // set a single map marker
     myMapApp.setMapMarker = function (place) {
         var placeType = place.types[0];
+        //console.log("placeType = ", placeType);
         // find the place icon file address located in the googleTypes array
-        for (var i = 0; i < myMapApp.googleTypes.length; i++) {
-          if (placeType == myMapApp.googleTypes[i]) {
+        for (var i = 0; i < myMapApp.placeTypes().length; i++) {
+          //console.log("placeType = ", placeType);
+          //console.log("myMapApp.placeTypes()[i].googleType = ", myMapApp.placeTypes()[i].googleType);
+          if (placeType == myMapApp.placeTypes()[i].googleType) {
             var placeIconFile = myMapApp.placeTypes()[i].placeIcon;
             break;
           }
@@ -271,6 +373,7 @@ myMapApp.viewModel = function() {
           console.log("Failed to getFlickrPhotos, error = ", error);
         });
 
+
       // get Flickr image html URL
       function getImages(data) {
         var htmlPhotoURLs = [];
@@ -285,6 +388,7 @@ myMapApp.viewModel = function() {
         return htmlPhotoURLs;
       };
 
+
       // store images in flickrPhotos array
       function storeImages(imagesArray) {
         var imagesRecord = {};
@@ -298,37 +402,37 @@ myMapApp.viewModel = function() {
 
     // Filter the search query field
     myMapApp.query.subscribe (function(newValue) {
+      console.log("query.subscribe, newValue = ", newValue);
+      if (newValue.length > 0) {
+        myMapApp.setCheckedPlaceTrue();
+        myMapApp.setCheckedPlaceDisabled(true);
+      } else {
+        myMapApp.setCheckedPlaceDisabled(false);
+      }
+
+      // set all place types in legend to TRUE if non-blank search query
+      // if (myMapApp.query() != "") {
+      //       myMapApp.setCheckedPlaceTrue();
+      // }
       // use knockout to filter the Complete List and create the Computed List
       myMapApp.ComputedList = ko.computed(function() {
+        console.log("Inside query.subscribe ComputedList def");
         var filter = myMapApp.query().toLowerCase();
         if (myMapApp.query() === "") {
             //console.log("No filter", filter);
             // return the complete list
             return myMapApp.CompleteList();
         } else {
+            //myMapApp.setCheckedPlaceTrue();
+            //myMapApp.setCheckedPlaceDisabled();
             return ko.utils.arrayFilter(myMapApp.CompleteList(), function(item) {
               //find the filter string in the name
               return item.name().toLowerCase().indexOf(filter)>-1;
             });
         }
       });
-      // Create the Filtered List by pushing data from the Computed List.
-      myMapApp.FilteredList.removeAll();
-      for (var i = 0; i < myMapApp.ComputedList().length; i++) {
-        myMapApp.FilteredList.push( myMapApp.ComputedList()[i] );
-      }
-      // Set the markers with the filtered locations
-      myMapApp.deleteMarkers();
-      myMapApp.bounds = new google.maps.LatLngBounds();
-      for (var i = 0; i < myMapApp.FilteredList().length; i++) {
-        var place = myMapApp.FilteredList()[i].place;
-        place.marker = myMapApp.setMapMarker(myMapApp.FilteredList()[i].place);
-        myMapApp.bounds.extend(new google.maps.LatLng(
-            place.geometry.location.lat(),
-            place.geometry.location.lng()));
-      }
-      // center the map
-      myMapApp.centerMap();
+      // display the Filtered List
+      myMapApp.displayFilteredList(myMapApp.ComputedList);
     });
 
 
@@ -390,8 +494,8 @@ myMapApp.viewModel = function() {
         photoURL = "No Flickr photos found for " + placeName + ".";
       }
       // find the display place type
-      for (var i = 0; i < myMapApp.googleTypes.length; i++) {
-          if (placeType == myMapApp.googleTypes[i]) {
+      for (var i = 0; i < myMapApp.placeTypes().length; i++) {
+          if (placeType == myMapApp.placeTypes()[i].googleType) {
             var placeTypeString = myMapApp.placeTypes()[i].displayPlaceType;
             break;
           }
